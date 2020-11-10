58fb27c2d1e4de38e27f56dbd50fc8f6f76f0da0
Wyatt Nolen <wyattnolen@gmail.com>
2020-11-09 20:23:32 -0500

    Download canvas

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -15,10 +15,24 @@ class Canvas extends React.Component {
     this.ctx.fillText(this.props.input, 210, 75);
   }
 
+  _downloadImage() {
+    // window.location.href = "data:image/png;base64," + this.canvas.toDataURL();
+    let fileName = "image";
+    const link = document.createElement("a");
+    link.download = fileName + ".png";
+    console.log(this.canvas);
+    this.canvas.toBlob(function (blob) {
+      console.log(blob);
+      link.href = URL.createObjectURL(blob);
+      link.click();
+    });
+  }
+
   render() {
     return (
       <div>
         <canvas ref={this.canvasRef} width={640} height={425} />
+        <p onClick={() => this._downloadImage()}>Download Image</p>
       </div>
     );
   }
